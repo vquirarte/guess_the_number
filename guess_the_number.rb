d5a9e74ef6c09e08e1dd3ca43a3cd5a1bd623611
vquirarte <vquirarte1121@icloud.com>
2014-03-18 18:05:55 +0000

    checked guess and secret number and returned message

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,3 +1,12 @@
 puts "Welcome to guess the number"
 secret_number = rand(6)
-puts secret_number
+
+puts "Guess a number between 0 and 5:"
+guess = gets.chomp.to_i
+
+if secret_number == guess
+  puts "Congratulations! You are correct"
+else
+  puts "Wrong! #{secret_number} was the correct number"
+end
+  
